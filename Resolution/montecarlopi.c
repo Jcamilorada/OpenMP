40ab52e6b9afb6e93f91e90153ada735d9a7f559
= <=>
2015-10-27 21:30:19 -0500

    Added solution

--- Resolution/montecarlopi.c
+++ Resolution/montecarlopi.c
@@ -1,77 +1,95 @@
 #include <stdio.h>   
 #include <omp.h>   
-   
-static long MULTIPLIER  = 1366;   
-static long ADDEND      = 150889;   
-static long PMOD        = 714025;   
-long random_last = 0.0;   
-double random_low, random_hi;   
 
-/*
-Functions used to generate random numbers
-*/
-double random()   
-{   
-    long random_next;
+
+static long MULTIPLIER = 1366;
+static long num_trials = 100000;
+static long ADDEND     = 150889;
+static long PMOD = 714025;
+static unsigned long long mult_n;
+
+#define MAX_THREADS 128
+static unsigned long long seed_array[MAX_THREADS];
+
+unsigned long long random_last = 0;
+#pragma omp threadprivate(random_last)
+
+double random_low, random_hi;
+double random()
+{
+    unsigned long long random_next;
     double ret_val;
-   
-    random_next = (MULTIPLIER  * random_last + ADDEND)% PMOD;   
-    random_last = random_next;   
-   
-    ret_val = ((double)random_next/(double)PMOD) * (random_hi-random_low) + random_low;
+
+    random_next = (unsigned long long)((mult_n  * random_last + ADDEND)% PMOD);
+    random_last = random_next;
+
+    ret_val = ((double)random_next/(double)PMOD)*(random_hi-random_low)+random_low;
 
     return ret_val;
-}   
-
-void seed(double low_in, double hi_in)   
-{
-   if(low_in < hi_in)   
-   {
-      random_low = low_in;   
-      random_hi  = hi_in;   
-   }
-   else
-   {
-      random_low = hi_in;   
-      random_hi  = low_in;   
-   }
-
-   random_last = PMOD/ADDEND;
 }
 
-//
-// The monte carlo pi program   
-// Include the OpenMP directives to transform this sequential program in  a
-// parallel one
-//
-static long num_trials = 100000;
-   
-int main ()   
-{   
-   long i;  long Ncirc = 0;   
-   double pi, x, y, test;   
-   double r = 1.0;   // radius of circle. Side of squrare is 2*r    
-   
-   seed(-r, r);  // The circle and square are centered at the origin   
+void seed(double low_in, double hi_in)
+{
+    int i, id, nthreads;
+    unsigned long long iseed;
+    id = omp_get_thread_num();
 
-   #pragma omp parallel for private (x, y, test) reduction (+:Ncirc)
-   for(i=0; i < num_trials; i++)
-   {   
-      x = random();    
-      y = random();   
-   
-      test = x*x + y*y;   
-   
-      if (test <= r*r)
-      {
-          Ncirc++;
-      }
+    #pragma omp single
+    {
+        if (low_in < hi_in)
+        {
+            random_low = low_in;
+            random_hi = hi_in;
+        }
+        else
+        {
+            random_low = hi_in;
+            random_hi = low_in;
+        }
 
+        nthreads = omp_get_num_threads();
+        mult_n = MULTIPLIER;
+
+        iseed = PMOD / MULTIPLIER;
+        seed_array[0] = iseed;
+
+        for (i = 1; i < nthreads; ++i)
+        {
+            iseed = (unsigned long long) ((MULTIPLIER * iseed + ADDEND) % PMOD);
+            seed_array[i] = iseed;
+            mult_n = (mult_n * MULTIPLIER) % PMOD;
+        }
     }
 
-    pi = 4.0 * ((double)Ncirc/(double)num_trials);   
+    random_last = (unsigned long long) seed_array[id];
+}
 
-    printf("\n %d trials, pi is %f \n",num_trials, pi);   
 
-    return 0;   
-}   
+int main()
+{
+    long Ncirc = 0;
+    long i;
+    double pi, x, y;
+    double r = 1.0;
+
+    #pragma omp parallel
+    {
+        seed(-r, r);
+        #pragma omp for reduction (+:Ncirc) private (x, y)
+        for (i = 0; i < num_trials; i++)
+        {
+            x = random();
+            y = random();
+
+            if ((x * x + y * y) <= r * r)
+            {
+                Ncirc++;
+            }
+        }
+    }
+
+    pi = 4.0 * ((double) Ncirc / (double) num_trials);
+    printf("\n %d trials, pi is %f \n", num_trials, pi);
+
+    return 0;
+}
